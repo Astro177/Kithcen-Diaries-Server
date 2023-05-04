02928efd914c239e771214682d3478aaf38e6ad5
Astro <rgrxmahabub.official@gmail.com>
2023-05-04 09:26:39 +0600

    server functionality done

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ app.get("/", (req, res) => {
   res.json({ hey: "something is running" });
 });
 const chefDetails = require("./data_file/chefs.json");
+const recipeDetails = require("./data_file/recipe.json");
 
 app.get("/chefData", (req, res) => {
   res.send(chefDetails);
+});
+
+app.get("/chefData/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChef = chefDetails.find((chef) => chef.id === id);
+  res.send(singleChef);
+});
+
+app.get("/recipeData", (req, res) => {
+  res.send(recipeDetails);
 });
 
 app.listen(5000, () => {
